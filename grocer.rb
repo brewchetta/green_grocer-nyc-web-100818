66d7003e4b9da8a0b4a99435457a4d25e77dab8b
Chett Tiller <chett.tiller@gmail.com>
2018-09-26 17:50:24 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -40,6 +40,7 @@ end
 def apply_clearance(cart)
   cart.each do |food,value|
     cart[food][:price] = '%.2f' % (cart[food][:price] * 0.80) if cart[food][:clearance]
+    cart[food][:price] = cart[food][:price].to_f
   end
   cart
 end
