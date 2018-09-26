23d12e26a844762981789445f0776cd6885a102e
Chett Tiller <chett.tiller@gmail.com>
2018-09-26 17:08:31 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -27,6 +27,7 @@ def apply_coupons(cart, coupons)
     cart[item] == nil ? puts "You have a coupon but no item!" : cart[item][:count] -= coupons[:num]
     cart["#{item} W/COUPON"][:price] = coupon[:cost]
     cart["#{item} W/COUPON"][:clearance] = cart[item][:clearance]
+    cart["#{item} W/COUPON"][:count] == nil ? cart["#{item} W/COUPON"][:count] = 1 : cart["#{item} W/COUPON"][:count] += 1
   end
   puts cart
   cart
